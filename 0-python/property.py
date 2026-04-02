697d499fb81cff539f7e8890bb9a13ed4b3b0be0
rafsan <syed.raiyan@ucb.com.bd>
2026-04-02 16:28:34 +0600

    property in python

--- 0-python/property.py
+++ 0-python/property.py
@@ -1,25 +1,27 @@
 class Celsius:
     def __init__(self, temparature=0):
-        self.set_temparature(temparature)
+        self.temparature = temparature
 
     def to_fahrenheit(self):
         return (self._temparature * 1.8) + 32
 
-    def get_temparature(self):
+    @property
+    def temparature(self):
         print("Getting temperature...")
         return self._temparature
 
-    def set_temparature(self, value):
+    @temparature.setter
+    def temparature(self, value):
         print("Setting temperature...")
         if value < -273.15:
             raise ValueError("Temperature below absolute zero is not possible")
         self._temparature = value
 
-    # Creating a property object
-    temparature = property(get_temparature, set_temparature)
-
 human = Celsius(37)
-
 print(human.temparature)
 print(human.to_fahrenheit())
-human.temparature = -300
+
+try:
+    human.temparature = -300
+except ValueError as e:
+    print(e)
